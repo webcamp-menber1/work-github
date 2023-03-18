f5143f48fd75c4cd949258a2adfe2f3d00bcc185
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-03-18 08:53:38 +0000

    [add]publicのcontroller,views,routes

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,19 @@
+class Public::OrdersController < ApplicationController
+  def new
+  end
+  
+  def create
+  end
+  
+  def index
+  end
+
+  def show
+  end
+
+  def confirm
+  end
+
+  def done
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,31 @@
 Rails.application.routes.draw do
 
+  scope module: :public do
+    root to:'homes#top'
+    get 'about'=>'homes#about',as:'about'
+    
+    get 'customers/my_page'=>'/customers#show'
+    get 'customers/information/edit'=>'/customers#edit'
+    patch 'customers/information'=>'customers#update'
+    get 'customers/confirm'=>'customers#confirm'
+    patch '/customers/withdrawal'=>'customers#withdrawal'
+    
+    resources:items,only:[:index,:show]
+    resources:cart_items,only:[:index,:create,:update,:destroy] do
+      collection do
+        delete 'destroy_all'
+      end
+    end
+    resources:orders,only:[:new,:create,:index,:show] do
+      collection do
+        post 'confirm'
+        get  'done'
+      end
+    end
+    resources:addresses,only:[:create,:index,:edit,:update,:destroy]
+  end
+
+
   devise_for :customers,skip:[:passwords],controllers:{
     registrations:"public/registrations",
     sessions:'public/sessions'
