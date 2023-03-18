4b3892e1e3c04f1e8ee6b3ff4107a5e510df32c0
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-03-18 07:40:39 +0000

    [add]退会済み処理

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-
+    before_action:customer_state,only:[:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -26,5 +26,14 @@ class Public::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
+protected
 
+  def customer_state
+    @customer=Customer.find_by(email:params[:customer][:email])
+    return if!@customer
+
+    if @customer.valid_password?(params[:customer][:password]) && !@customer.is_deleted
+        redirect_to new_customer_registration_path
+    end
+  end
 end
