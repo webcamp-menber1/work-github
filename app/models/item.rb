1c603c393971e46d5497295daa7ed01e9fb1441d
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-03-22 09:17:45 +0000

    [add]商品詳細画面

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,11 +15,12 @@ class Item < ApplicationRecord
   enum is_active: {販売中:true,販売停止中:false}
 
   def get_image(width,height)
-    unless image_attached?
+    unless image.attached?
       file_path=Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io:File.open(file_path),filename:'default-image.jpg',content_type:'image/jpeg')
+      image.attach(io: File.open(file_path),filename:'default-image.jpg',content_type:'image/jpeg')
     end
     image.variant(resize_to_limit:[width,height]).processed
   end
+
 
 end
