afa2df74b90f1113a6964342021b195312e47b31
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-03-22 09:56:08 +0000

    [add]商品編集画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -22,9 +22,17 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item=Item.find(params[:id])
   end
 
   def update
+    @item=Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:notice]="変更に成功しました。"
+      redirect_to admin_item_path(@item.id)
+    else
+      render:edit
+    end
   end
 
   private
