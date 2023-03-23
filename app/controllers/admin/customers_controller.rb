b54f6cb0eebf96f1f7ccfd88889c7b0bf03c1c86
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-03-23 08:03:05 +0000

    [add]会員情報編集画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,8 +8,22 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer=Customer.find(params[:id])
   end
 
   def update
+    @customer=Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice]="変更に成功しました"
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render:edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number,:email,:is_deleted)
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,6 +8,14 @@ class Customer < ApplicationRecord
   has_many:orders,     dependent: :destroy
   has_many:addresses,  dependent: :destroy
 
+  validates :last_name,presence:true
+  validates :first_name,presence:true
+  validates :last_name_kana,presence:true
+  validates :first_name_kana,presence:true
+  validates :postal_code,presence:true
+  validates :address,presence:true
+  validates :telephone_number,presence:true
+  validates :is_deleted,inclusion: ["有効","退会"]
 
   enum is_deleted: {有効:false,退会:true}
 end
