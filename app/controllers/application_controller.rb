da9eebdacd922de466fb15fbdb255de1cf118f19
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-03-31 05:14:49 +0000

    [

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-    # before_action:authenticate_admin!,except:[:about]
+    #before_action:authenticate_admin!,except:[:about]
     # before_action:authenticate_customer!,except:[:top,:about]
 end
