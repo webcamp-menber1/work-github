b2ee3dd38c0d4fb790ce603625ccfdf711acc058
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-04-01 06:38:30 +0000

    [add]マイページ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,16 +1,17 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer=Customer.find(current_customer.id)
   end
 
   def edit
   end
-  
+
   def update
   end
-  
+
   def confirm
   end
-  
+
   def withdrawal
   end
 end
