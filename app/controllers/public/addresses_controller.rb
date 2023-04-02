875bc052aee939a731ead7607bedb46e3621d1ea
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-04-02 02:10:58 +0000

    [add]配送先登録/一覧　新規登録機能

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,18 +1,34 @@
 class Public::AddressesController < ApplicationController
-  
+
   def create
+    @address=Address.new
+    @address.customer_id==current_customer.id
+    if @address.save
+      flash[:notice]="登録に成功しました"
+      redirect_to '/addresses'
+    else
+      render:index
+    end
   end
-  
+
   def index
+    @address=Address.new
+    @addresses=Address.all
   end
 
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
+
+  private
+
+  def address_params
+    params.require(:address).permit(:customer_id,:name,:postal_code,:address)
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ApplicationRecord
   belongs_to:customer
+
+  validates:customer_id,presence:true
+  validates:name,presence:true
+  validates:postal_code,presence:true
+  validates:address,presence:true
+
+
 end
+
+
