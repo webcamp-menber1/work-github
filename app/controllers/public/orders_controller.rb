dbfbe2918b9dc405595a941a82ef503977f1a438
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-04-05 08:07:26 +0000

    [add] 注文情報確認画面の支払方法、お届け先、注文商品表示、請求額表示

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -13,6 +13,23 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @cart_items=current_customer.cart_items
+    @total=0
+    @postage=800
+    @order=Order.new(order_params)
+    @order.customer_id=current_customer.id
+    if params[:order][:select_address]==0
+      @order.postal_code=current_customer.postal_code
+      @order.address=current_customer.address
+      @order.name=current_customer.last_name+current_customer.first_name
+    elsif params[:order][:select_address]==1
+      @address=Address.find(params[:order][:address_id])
+      @order.postal_code=@address.postal_code
+      @order.address=@address.address
+      @order.name=@address.name
+    else
+
+    end
   end
 
   def done
