0025ec7b343a0d839e74f815f0770f868672b42f
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-04-07 08:35:17 +0000

    [add]注文履歴詳細画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -29,6 +29,9 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+    @order=Order.find(params[:id])
+    @order_details=@order.order_details
+    @total=0
   end
 
   def confirm
