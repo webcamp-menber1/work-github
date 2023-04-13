0f46c5cd31822f788dfddb283750af18b46eb8e4
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-04-13 06:13:27 +0000

    [add]管理者側ページアクセス制限（管理者ログイン時のみ閲覧可能）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-    #before_action:authenticate_admin!,except:[:about]
-    # before_action:authenticate_customer!,except:[:top,:about]
+    #before_action:authenticate_admin!
+    #before_action:authenticate_customer!,except:[:top,:about]
 
 
 
